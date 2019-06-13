1e436987357dcd4c20b2910fb849045ec0364e65
Lisa <lmkirk@gmail.com>
2019-06-13 15:10:10 -0400

    final-commit. Project completed.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -93,6 +93,7 @@ let ticketPriceTotal = [];
     }, 0);
 console.log(ticketPriceTotal);
 
+
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
