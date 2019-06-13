58d69e1eacb2e8d36caa54c1bbdd86293d0a2920
Lisa <lmkirk@gmail.com>
2019-06-13 11:44:49 -0400

    function contains completed with for loop

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,30 +26,45 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    return cb = arr.length;   // getLength passes the length of the array into the callback.
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    // last passes the last item of the array into the callback.
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb = x + y;// sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb = x * y;// multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
 }
+        for (let i=0; i<items.length; i++) {
+            if (list[i] === item) {
+                return cb = true;
+            }
+            else {
+                return false;
+            }
+        }
+        return cb;
+    }
+
+// invoke contains()
+    console.log(contains('Pencil', items));
+    console.log(contains('Sharpie', items));
+
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+    // removeDuplicates removes all duplicate values from the given array.
+    // Pass the duplicate free array to the callback function.
+    // Do not mutate the original array.
 }
